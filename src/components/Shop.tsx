d845abcc0daaf07e88826aa102bee93e496a7a65
soulfeelings <sergey.prilepcko@gmail.com>
2025-02-20 01:58:21 +0500

    scroling

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,13 +1,30 @@
 import { pb, useConfiguratorStore } from "@/store";
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 import { AssetCard, NoAssetCard } from "./AssetCard";
 import { useCanAffordSelected } from "@/hooks/use-can-afford-selected";
 import { Space } from "./Space";
 
+const throttle = (func: (...args: any[]) => void, limit: number) => {
+    let inThrottle: boolean;
+    return function() {
+        const args = arguments;
+        const context = this;
+        if (!inThrottle) {
+            func.apply(context, args);
+            inThrottle = true;
+            setTimeout(() => inThrottle = false, limit);
+        }
+    };
+};
+
 export const Shop: React.FC = () => {
     const categories = useConfiguratorStore((state) => state.categories);
     const assets = useConfiguratorStore((state) => state.assets);
+    const [translate, setTranslate] = useState(0);
+    const [hideUpButton, setHideUpButton] = useState(false);
+    const [hideDownButton, setHideDownButton] = useState(true);
+    const scrollContainerRef = useRef<HTMLDivElement>(null);
     const setCurrentCategory = useConfiguratorStore((state) => state.setCurrentCategory);
     const setSelectedAsset = useConfiguratorStore((state) => state.setSelectedAsset);
     const isMobile = useConfiguratorStore((state) => state.isMobile);
@@ -39,34 +56,79 @@ export const Shop: React.FC = () => {
         }
     }, [categories]);
 
+    const scrollContainerHeight = scrollContainerRef.current?.getBoundingClientRect().height ?? 0;
+
+    const throttleScroll = useCallback(throttle((e: WheelEvent) => {
+        setTranslate(prev => {
+            const next = prev + (e.deltaY * 3);
+            if (next > 0) {
+                setHideUpButton(false);
+                return 0;
+            }
+
+            if (next < -(scrollContainerHeight - 400)) {
+                setHideDownButton(true);
+                return -(scrollContainerHeight - 400);
+            }
+
+            setHideDownButton(next < -(scrollContainerHeight - 400));
+            setHideUpButton(next > 0);
+            return next;
+        });
+    }, 100), [scrollContainerHeight]);
+
     return (
         <div className="flex flex-col h-full">
             <div className={`pointer-events-auto relativep-[25px] max-w-lg mx-auto flex flex-col bg-[#DFD1C1]/30 ${isMobile ? "min-w-[300px] min-h-[300px] w-[300px] h-[300px]" : "min-w-[500px] min-h-[500px] w-[500px] h-[500px]"} rounded-[50px]`}>
                 {!isMobile && <h1 className="text-2xl font-bold text-center mb-4 color-[#333333] mt-[29px]">{currentCategory?.name}</h1>}
                 <div className="flex flex-col gap-2 translate-x-[-100%] flex-shrink-0 absolute top-[25px] left-0">
                     {/* button to scroll up */}
-                    <button className="">
+                    <button className="pointer-events-auto" style={{ opacity: hideUpButton ? 0 : 1, pointerEvents: hideUpButton ? "none" : "auto" }} onClick={() => setTranslate((prev) => {
+                        const next = prev - 100;
+                        setHideDownButton(false);
+
+                        if (next < -(scrollContainerHeight - 400)) {
+                            setHideUpButton(true);
+                            return -(scrollContainerHeight - 400);
+                        }
+                        return next;
+                    })}>
                         Up
                     </button>
-                    <div className="flex flex-col gap-2 overflow-y-auto h-[400px]">
-                        {categories.map((category) => (
-                            <button
-                                key={category.id}
-                                onClick={() => setCurrentCategory(category)}
-                                className={`${isMobile ? "" : "h-[60px] min-h-[60px] w-[80px]"} 
-                                    p-[15px] rounded 
-                                    text-[0.5em]
-                                    relative 
-                                    ${currentCategory?.id === category.id ? "bg-[#FFBB4E] text-white" : "bg-[#E8E3D9]"} 
-                                    [clip-path:polygon(0%_0%,100%_0%,100%_100%,0%_100%,10%_50%)]`}
-                            >
-                                {category.image ? <img src={pb.files.getUrl(category, category.image)} alt={category.name} className="w-full h-full object-contain" /> : category.name}
-                            </button>
+                    <div className="overflow-y-hidden h-[400px]" >
+                        <div ref={scrollContainerRef} onWheel={(e) => {
+                            // throttle
+                            throttleScroll(e);
+                        }} className="flex flex-col gap-2 transition-transform duration-300" style={{ transform: `translateY(${translate}px)` }}>
+                            {categories.map((category) => (
+                                <button
+                                    key={category.id}
+                                    onClick={() => setCurrentCategory(category)}
+                                    className={`${isMobile ? "" : "h-[60px] min-h-[60px] w-[80px]"} 
+                                        p-[15px] rounded 
+                                        text-[0.5em]
+                                        relative 
+                                        ${currentCategory?.id === category.id ? "bg-[#FFBB4E] text-white" : "bg-[#E8E3D9]"} 
+                                        [clip-path:polygon(0%_0%,100%_0%,100%_100%,0%_100%,10%_50%)]`}
+                                >
+                                    {category.image ? <img src={pb.files.getUrl(category, category.image)} alt={category.name} className="w-full h-full object-contain" /> : category.name}
+                                </button>
 
-                        ))}
+                            ))}
+                        </div>
                     </div>
                     {/* button to scroll down */}
-                    <button className="">
+                    <button className="pointer-events-auto" style={{ opacity: hideDownButton ? 0 : 1, pointerEvents: hideDownButton ? "none" : "auto" }} onClick={() => setTranslate((prev) => {
+                        const next = prev + 100;
+                        setHideUpButton(false);
+
+                        if (next > 0) {
+                            setHideDownButton(true);
+                            return 0;
+                        }
+
+                        return next;
+                    })}>
                         Down
                     </button>
                 </div>
